fd0d3262b745f4ac4283d0a1a1c2e4f4bd74aaaa
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-04 23:58:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ new_collection = []
+ while i < colleciton.length
+   if yield(collection[i]) == true do
+     new_collection << yield (collection[i])
+   end
+   i += 1
+ end 
 end
